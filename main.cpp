d3f401fa6e204c25dfc49fd0736c311c20c48b67
asael <asael.herrera@utec.edu.pe>
2026-05-05 00:02:21 -0500

    a

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main() {
 
     }
     std::cout<<"hola";
-    std::cout<<"hello";
+    std::cout<<"a";
     return 0;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
