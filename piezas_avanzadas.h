ec48dfe19866ddaf9650dbb3c5f895d841188995
Erosadio <adrian.rosadio@utec.edu.pe>
2026-05-07 10:42:09 -0500

    Comienzo de las piezas avanzadas

--- piezas_avanzadas.h
+++ piezas_avanzadas.h
@@ -1,8 +1,20 @@
-//
-// Created by 3scar on 7/05/2026.
-//
-
 #ifndef AJEDREZ_PIEZAS_AVANZADAS_H
 #define AJEDREZ_PIEZAS_AVANZADAS_H
+
+#include "constantes.h"
+
+// Flags necesarios para cumplir enroque
+extern bool reyBlancoMovido;
+extern bool reyNegroMovido;
+extern bool torreBlancaA_Mov;
+extern bool torreBlancaH_Mov;
+extern bool torreNegraA_Mov;
+extern bool torreNegraH_Mov;
+
+// Movimientos de piezas
+bool validarRey (char tab[8][8], int fo, int co, int fd, int cd);
+bool validarCaballo (char tab[8][8], int fo, int co, int fd, int cd);
+bool validarDama (char tab[8][8], int fo, int co, int fd, int cd);
+
 
 #endif //AJEDREZ_PIEZAS_AVANZADAS_H
