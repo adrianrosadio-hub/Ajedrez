06ae0f07fd0a16b0e96988d272e026856a0fc13e
Erosadio <adrian.rosadio@utec.edu.pe>
2026-05-07 11:17:33 -0500

    avance de piezas_avanzadas.h

--- piezas_avanzadas.h
+++ piezas_avanzadas.h
@@ -1,6 +1,5 @@
 #ifndef AJEDREZ_PIEZAS_AVANZADAS_H
 #define AJEDREZ_PIEZAS_AVANZADAS_H
-
 #include "constantes.h"
 
 // Flags necesarios para cumplir enroque
@@ -15,6 +14,12 @@ extern bool torreNegraH_Mov;
 bool validarRey (char tab[8][8], int fo, int co, int fd, int cd);
 bool validarCaballo (char tab[8][8], int fo, int co, int fd, int cd);
 bool validarDama (char tab[8][8], int fo, int co, int fd, int cd);
+
+// Intentar enroque
+bool intentarEnroquer (char tab[8][8], char color, bool corto);
+
+// Coordinador general
+bool esMovimientoValido(char tab[8][8], int fo, int co, int fd, int cd, int turno);
 
 
 #endif //AJEDREZ_PIEZAS_AVANZADAS_H
